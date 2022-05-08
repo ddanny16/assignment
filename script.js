ea2224eac16dac7891352152db1be32115d3724a
ddanny16 <102356223+ddanny16@users.noreply.github.com>
2022-05-08 20:27:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var age = 21;
+var Name = "Brahdah";
+var online = true;
+var letme = null;
+var carName1 = "honda XC50"
+
+// letme=assignment
+
+console.log("i am " ,age);
+console.log("welcome",Name);
+console.log("You are online:",online)
+console.log(letme)
+
