0a20db33cf6131b649f0f5c3ee4b35b227602b17
ddanny16 <102356223+ddanny16@users.noreply.github.com>
2022-05-08 21:33:28 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
 # assignment
- created 6 variables
- each named differentely
- the number variable with :age;
- the string variable with :bradah;
- thee boolean variable with:true;
-an empty variable
-an array variable with phone options
-[iphone,nokia,infinix]
-
-began to log data types to console
-still logging each data types to each and different consoles 
-finished logging variables to consoles
--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-var age = 21;
+var age = 21;  
+// these are numeric or number variables containig numbers
 var Name = "Brahdah";
+// these are string variables containing alphabet
 var online = true;
+// this is a boolean variable to tend represent one of 2 values
 var letme = null;
+// empty variable containing nothing
 var carName1 = "honda XC50"
+// object variable that assigns a simple value to a variable
+var phones =["iphone","nokia","infinix"]
+// array variable store multiple values in a single variable
 
 // letme=assignment
+age = age + 4;
 
 console.log("i am " ,age);
 console.log("welcome",Name);
-console.log("You are online:",online)
-console.log(letme)
-
+console.log("You are online:",online);
+console.log(letme);
+console.log("your car is:",carName1);
+console.log("phone options",phones)
